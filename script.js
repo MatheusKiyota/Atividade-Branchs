a7fdf8a88b7d568b7720d510cf74c462fb36db38
MatheusKiyota <matheus.kiyota@estudante.ifms.edu.br>
2025-09-23 13:58:04 -0400

    css e função janela

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 document.getElementById('alertButton').addEventListener('click', function() {
-    alert('Você clicou no botão de Alerta!');
+    const modal = document.getElementById('myModal');
+    modal.style.display = 'block';
 });
 
+
+document.getElementById('closeModal').addEventListener('click', function() {
+    const modal = document.getElementById('myModal');
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+    const modal = document.getElementById('myModal');
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
 
